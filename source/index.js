c769c9099fb5f9df0dd832b594caa9f7ae079dde
Q-Alecsey <alecsey.kotelnikov@gmail.com>
2025-06-11 22:41:42 +0300

    slider-d

--- source/index.js
+++ source/index.js
@@ -1,7 +1,32 @@
-//
 
-function func() {
-    alert("oh yeah");
+const swiper = new Swiper('.trainers__swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop:true,
+  slidesPerView:3,
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+const this_swiper = document.querySelector('.trainers__swiper').swiper;
+
+function Next(){
+    
+    if (this_swiper.activeIndex == 4){
+      this_swiper.slideTo(0);
+    }
+    else{
+      this_swiper.slideNext();
+    }
+    console.log(this_swiper.activeIndex);
 }
 
-document.getElementById("btn").onclick = func;
+console.log(this_swiper.scrollbar);
+
+
+
+
+
