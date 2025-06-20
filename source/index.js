1f22cf19353d294f33f5fbfbbc684f18f4f08845
Q-Alecsey <alecsey.kotelnikov@gmail.com>
2025-06-20 15:42:16 +0300

    responsive

--- source/index.js
+++ source/index.js
@@ -1,9 +1,12 @@
 
+import {data, info} from "./trainersData.js";
+
 const swiper = new Swiper('.trainers__swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop:true,
   slidesPerView:3,
+  spaceBetween:50,
 
   // And if we need scrollbar
   scrollbar: {
@@ -13,19 +16,124 @@ const swiper = new Swiper('.trainers__swiper', {
 
 const this_swiper = document.querySelector('.trainers__swiper').swiper;
 
-function Next(){
+document.querySelector('.swiper_button-next').onclick = 
+() => this_swiper.slideNext();
+
+document.querySelector('.swiper_button-prev').onclick = 
+() => this_swiper.slidePrev();
+
+
+//Modal_window
+
+const modal_window = document.querySelector(".trainers__dialog");
+
+//dialog__educaton-content   |||
+const dialogContent = document.querySelectorAll('.dialog__education-content'); 
+
+const dialogImage = document.querySelector('.dialog__header img');
+const dialogName = document.querySelector('.dialog__header-info h3');
+const dialogProf = document.querySelector('.dialog__header-info p');
+
+for (let item of document.querySelectorAll(".trainers__person-detailed")){
+  item.onclick = () => {
+
+    let name_attr = item.attributes["name"].value;
+
+    dialogImage.setAttribute("src", info[name_attr]["image"]);
+    dialogName.innerHTML = info[name_attr]['name'];
+    dialogProf.innerHTML = info[name_attr]['prof'];
+
+    let index = 0;
+    dialogContent.forEach(item => {
+        item.innerHTML = data[name_attr][index++];
+    })
     
-    if (this_swiper.activeIndex == 4){
-      this_swiper.slideTo(0);
-    }
-    else{
-      this_swiper.slideNext();
-    }
-    console.log(this_swiper.activeIndex);
+    modal_window.classList.add("trainers__dialog-on");
+  }
 }
 
-console.log(this_swiper.scrollbar);
+//BUTTONS EDUCATION EXPERIENCE  AWARDS
 
+function deleteClassForAll(listEls, className){
+    for (let item of listEls){
+        item.classList.remove(className);
+    }
+}
+
+function setStyleForAll(listEls, styleName, value){
+    for (let item of listEls){
+        item.style[styleName] = value;
+    }
+}
+
+const btn_education = document.querySelector('.dialog__tabs-item.education');
+const btn_exp = document.querySelector('.dialog__tabs-item.experience');
+const btn_awards = document.querySelector('.dialog__tabs-item.awards');
+
+const dialog_education = document.querySelector('.dialog__education');
+const dialog_exp = document.querySelector('.dialog__experience');
+const dialog_awards = document.querySelector('.dialog__awards');
+
+const list_btns = [btn_education, btn_exp, btn_awards];
+const list_dialog = [dialog_education, dialog_exp, dialog_awards];
+
+//
+btn_education.onclick = () => {
+  deleteClassForAll(list_btns, "active");
+
+  btn_education.classList.add("active");
+  setStyleForAll(list_dialog, "display", "none");
+
+  dialog_education.style.display = "block";
+}
+
+//
+btn_exp.onclick = () => {
+  deleteClassForAll(list_btns, "active");
+
+  btn_exp.classList.add("active");
+  setStyleForAll(list_dialog, "display", "none");
+
+  dialog_exp.style.display = "block";
+}
+ 
+//
+btn_awards.onclick = () => {
+  deleteClassForAll(list_btns, "active");
+
+  btn_awards.classList.add("active");
+  setStyleForAll(list_dialog, "display", "none");
+
+  dialog_awards.style.display = "block";
+}
+
+//close modal_window
+
+document.querySelector(".dialog__header-close").onclick = () => {
+  modal_window.classList.remove("trainers__dialog-on");
+
+  setTimeout(() => {
+    deleteClassForAll(list_btns, "active");
+
+    btn_education.classList.add("active");
+    setStyleForAll(list_dialog, "display", "none");
+
+    dialog_education.style.display = "block";
+  }, 700);
+
+}
+
+//course__form input hover
+
+const input = document.querySelectorAll('.course__form input')[1];
+
+input.addEventListener('mouseover', () => {
+  input.placeholder = '+7(___)___-__-__';
+});
+
+input.addEventListener('mouseout', () => {
+  input.placeholder = "Телефон";
+});
 
 
 
---
+++ source/trainersData.js
@@ -0,0 +1,98 @@
+
+export let data = {
+    Irina:["Сентябрь 1998 — Июнь 2003<br>"+
+        "Санкт-Петербургский политехнический университет Петра Великого<br>"+
+        "Факультет: Компьютерных наук и технологий<br>"+
+        "Специальность: Математика и компьютерные науки<br>"+
+        "Форма обучения: Очная",
+
+        "Ноябрь 2020 — Февраль 2021<br>"+
+        "Программа дополнительного образования «3D Моделирование»<br>"+
+        "Место проведения: Институт дополнительного образования «Политех»</br>",
+
+        "Опыт работы в ВК 3-4 года",
+
+        "Обладательница серебренной медали по программированию FEST 2020"
+    ],
+
+    Marina:["Сентябрь 1996 — Август 2000<br>"+
+        "МГУ имени Ломоносова<br>"+
+        "Факультет: Компьютерных наук и технологий<br>"+
+        "Специальность: Прикладная математика и информатика<br>"+
+        "Форма обучения: Очная",
+
+        "Ноябрь 2016 — Декабрь 2018<br>"+
+        "Программа дополнительного образования «Робототехника»<br>"+
+        "Место проведения: Институт дополнительного образования «Политех»</br>",
+
+        "Опыт работы в Яндекс 3 года",
+
+        "Обладательница серебренной медали по робототехнике FEST 2020"],
+
+    Maxim:["Some information about Maxim",
+
+        "Some information about Maxim",
+
+        "Maxim's Experience",
+
+        "Maxim's Awards"],
+
+    Constantin:["Some information about Constantin",
+
+        "Some information about Constantin",
+
+        "Constantin's Experience",
+
+        "Constantin's Awards"],
+    
+    Liza:["Some information about Liza",
+
+        "Some information about Liza",
+
+        "Liza's Experience",
+
+        "Liza's Awards"]
+}
+
+export let info = {
+    Irina:{
+        name:"Ирина Лайм",
+        prof:"преподаватель по робототехнике",
+        image:"images/trainers/person1.png",
+        facebook:"#",
+        twitter:"#"
+    },
+
+    Marina:{
+        name:"Марина Орлова",
+        prof:"преподаватель по робототехнике",
+        image:"images/trainers/person2.png",
+        facebook:"#",
+        twitter:"#"
+    },
+
+    Maxim:{
+        name:"Максим Петров",
+        prof:"преподаватель по программированию",
+        image:"images/trainers/person3.png",
+        facebook:"#",
+        twitter:"#"
+    },
+
+    Constantin:{
+        name:"Константин Назаров",
+        prof:"преподаватель по робототехнике",
+        image:"images/trainers/person4.png",
+        facebook:"#",
+        twitter:"#"
+    },
+    
+    Liza:{
+        name:"Лиза Весенняя",
+        prof:"преподаватель по программированию",
+        image:"images/trainers/person5.png",
+        facebook:"#",
+        twitter:"#"
+    }
+}
+
